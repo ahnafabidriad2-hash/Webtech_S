f9a6e1614642ca048bea367782d6c54212e42761
Ahnaf Abid Riad <ahnafabidriad2@gmail.com>
2026-05-04 19:59:37 +0600

    AjaxJson_Finaltask

---
+++ fetch_student_data.php
@@ -0,0 +1,11 @@
+<?php
+
+$student = array(
+    "name" => "Ahnaf Abid Riad",
+    "id" => "23-54594-3",
+    "department" => "Computer Science",
+    "cgpa" => "3.85"
+);
+
+echo json_encode($student);
+?>
